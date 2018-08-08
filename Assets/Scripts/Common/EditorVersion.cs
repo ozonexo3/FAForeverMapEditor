9cf3fc720e5da049add9431610a62ee089ff4133
ozonexo3 <ozonexo3@gmail.com>
2018-08-08 08:36:42 +0200

    v0.604

--- Assets/Scripts/Common/EditorVersion.cs
+++ Assets/Scripts/Common/EditorVersion.cs
@@ -7,8 +7,8 @@ public class EditorVersion : MonoBehaviour
 {
 
 	public const string EditorBuildVersion = "v0.604-Alpha";
-	//public const float VersionOffset = 0f; // Release
-	public const float VersionOffset = -0.001f; // Prerelease
+	public const float VersionOffset = 0f; // Release
+	//public const float VersionOffset = -0.001f; // Prerelease
 	//public const float VersionOffset = 0.523f - 0.600f; // Prerelease v0600
 	public static string LatestTag = "";
 	public static string FoundUrl;
